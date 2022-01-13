4e1b470972c3781b387ae196fe40014aaac5d2d7
Blvisse <blaisepke@gmail.com>
2022-01-13 20:32:33 +0300

    updated scripts with comments

--- data/fake_dates.py
+++ data/fake_dates.py
@@ -18,10 +18,14 @@ data.dropna(inplace=True,axis=0)
 print(data)
 Faker.seed(0)
 
+#generate fake dates between 1/1/1997 and 1/1/2020
+
 data['Date_of_registration']=[ faker.date_between(start_date="-25y", end_date="today") for i in range(len(data))]
     
 
 print(data)
 
+
+#save the data into a new csv file
 data.to_csv('date_data.csv',index=False)
 print("Saved to CSV file")
